ba49290913286a33b8a8f5ce712f2ac33942847b
xubo <750983498@qq.com>
2019-09-14 14:32:28 +0800

    feature:init

---
+++ app/src/main/java/com/shuisechanggong/qrcode/shell/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shuisechanggong.qrcode.shell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shuisechanggong.qrcode.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
